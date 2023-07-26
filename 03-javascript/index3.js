249e13f53a68df03024c99793fc556958ab2ac0a
chogain <chogain05@gmail.com>
2023-07-26 14:53:51 +0900

    study: js 자료형, 형변환

--- 03-javascript/index3.js
+++ 03-javascript/index3.js
@@ -117,3 +117,140 @@ let temp = null;
 console.log(temp);
 temp = 'hi'; // 추후 할당 가능
 console.log(temp);
+
+// 6. array (배열)
+// 배열의 이름 : fruits
+// 배열의 원소(아이템) : 배열 안에 있는 데이터 하나하나
+// 배열의 위치(index) : 0부터 시작
+// 배열의 길이(크기) : 원소의 개수와 동일
+const fruits = ['orange', 'pineapple', 'grape', 'apple'];
+// 배열을 생성하는 방법 2
+// const fruits2 = new Array('orange', 'pineapple', 'grape', 'apple');
+console.log(fruits); // ['orange', 'pineapple', 'grape', 'apple']
+console.log(fruits[0]); // fruits 배열에서 위치가 0인 원소 출력 -> orange 출력
+
+// js에서는 배열 원소의 자료형이 달라도 됨
+const data = [1, 'allie', false, null, undefined];
+console.log(data);
+
+// array 안에 array 들어갈 수 있음 (중첩 가능) -> 2차원 배열
+const korean = [
+  ['가', '나', '디'],
+  ['라', '마', '바'],
+  ['시', '아', '자'],
+];
+console.log(korean);
+console.log(korean[0]); // [['가', '나', '디']] 출력
+console.log(korean[1]);
+console.log(korean[2]);
+
+console.log(korean[0][2]); // '다' 출력
+
+// 퀴즈 korean 배열에서 '가자' 글씨 출력하기
+console.log(korean[0][0] + korean[2][2]);
+
+// 퀴즈 3차원 배열에서 숫자 8 출력
+const nums = [
+  [
+    [1, 2, 3],
+    [4, 5, 6],
+  ],
+  [
+    [7, 8, 9],
+    [10, 11, 12],
+  ],
+];
+
+console.log(nums[1][0][1]);
+
+// 7. object (배열)
+// {
+//   key: value;
+// }
+// 형태;
+
+let cat = {
+  name: '나비',
+  age: 1,
+  isCute: true,
+  meow: function () {
+    return '냐옹';
+  },
+};
+
+console.log(cat); // object 자체를 출력
+
+// object의 속성(key)에 접근하는 방법
+// 1. 점 표기법 (객체.key)
+console.log(cat.name);
+console.log(cat.age);
+console.log(cat.isCute);
+console.log(cat.meow());
+//2. 대괄호 표기법 (객체['key'])
+console.log(cat['name']);
+// 자주 사용하지 않지만 점 표기법을 사용하지 못하는 경우 사용
+// key가 변수에 저장되어 있을 때 주로 사용
+const tempVal = 'name';
+console.log(cat[tempVal]); // -> cat['name'] -> cat.name 동일
+
+// 자료형을 알려주는 typeof
+console.log(typeof '문자'); // string
+console.log(typeof 12345); // number
+console.log(typeof 3.14); // number
+console.log(typeof true); // boolean
+console.log(typeof false); // boolean
+console.log(typeof null); // object -> js 공식적으로 인정한 언어 자체의 오류
+console.log(typeof undefined); // object
+console.log(typeof cat); // object
+
+// 형변환
+// 성적 평균 구하는 프로그램 만들기
+// let mathScore = prompt('수학점수 입력');
+// console.log(mathScore, typeof mathScore);
+
+// let engScore = prompt('영어점수 입력');
+// console.log(engScore, typeof engScore);
+
+// let avg = Number(Number(mathScore) + Number(engScore)) / 2;
+
+// console.log(`수학과 영어의 평균 점수는 ${avg}점 입니다.`);
+
+// 1. 문자로 형변환
+
+// 1-1. String()
+let str1 = true;
+let str2 = 123;
+let str3 = null;
+
+console.log(String(str1), typeof String(str1)); // true string 출력
+console.log(String(str2), typeof String(str2)); // 123 string 출력
+console.log(String(str3), typeof String(str3)); // null string 출력
+
+// 1-2. .toString()
+console.log(str1.toString(), typeof str1.toString()); // true string 출력
+console.log(str2.toString(), typeof str2.toString()); // 123 string 출력
+
+// 2. 숫자로 형변환
+
+// 2-1. Number()
+let n1 = true;
+let n2 = false;
+let n3 = '123.9';
+
+console.log(Number(n1), typeof n1); // 1 -> true는 숫자로 변환 시 1
+console.log(Number(n2), typeof n2); // 0 -> false는 숫자로 변환시 0
+console.log(Number(n3), typeof n3); // '123.9'
+
+// 2-2. parseInt(변수명, 변환할) -> parseInt(변환할값) 가능
+// 문자열의 인자를 두 번째 인수의 진수인 정수로 변환
+
+console.log(parseInt(n3, 10)); // n3 값을 10진수의 정수(int)로 변환 -> 123
+console.log(parseInt(n3)); // 위와 동일
+
+// 2-3. parseFloat(변수명, 변환할진수) -> parseFloat(변환할값) 가능
+console.log(parseFloat(n3)); // n3 값을 실수(float)로 변환 - 123.9
+console.log(parseFloat(n3)); // 위와 동일
+
+// +) null, undefined를 숫자로 바꾼다면?
+console.log(Number(undefined), typeof Number(undefined));
+console.log(Number(null), typeof Number(null));
