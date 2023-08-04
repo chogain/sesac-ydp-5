f52557b90e4e3fe12ba403b0d9265530817cfefb
chogain <chogain05@gmail.com>
2023-08-04 17:30:42 +0900

    feat: 백드롭 영역 클릭 시 사이드바 숨기기

--- project_1/project_01.js
+++ project_1/project_01.js
@@ -1,3 +1,11 @@
-document.querySelector('.hamburgerIcon').addEventListener('click', () => {
-  document.querySelector('.backdrop').classList.toggle('d-none');
+$('.hamburgerIcon').on('click', () => {
+  $('aside').toggleClass('d-none');
+  $('main').toggleClass('back-drop');
+});
+
+$('main').on('click', () => {
+  if ($('main').hasClass('back-drop')) {
+    $('aside').toggleClass('d-none');
+    $('main').toggleClass('back-drop');
+  }
 });
