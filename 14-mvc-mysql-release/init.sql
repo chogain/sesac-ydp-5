6aefd1dfbadaf06371f9d08504fc4431ecfc8c77
chogain <chogain05@gmail.com>
2023-08-28 18:57:43 +0900

    studey: db 생성

--- 14-mvc-mysql-release/init.sql
+++ 14-mvc-mysql-release/init.sql
@@ -1,6 +1,8 @@
 -- 데이터베이스 목록 확인
 SHOW databases;
 
+CREATE DATABASE codingon;
+
 -- codingon 데이터베이스 선택
 USE codingon;
 
@@ -11,6 +13,12 @@ SHOW tables;
 DROP TABLE IF EXISTS user;
 
 -- TODO: user 데이터베이스 생성
+CREATE TABLE user (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  userid VARCHAR(20) NOT NULL,
+  name VARCHAR(10) NOT NULL,
+  pw VARCHAR(20) NOT NULL
+)
 
 -- user 데이블 데이터 추가
 INSERT INTO user (userid, name, pw) VALUES ('sean', 'sean', '1234');
