93ea8035589327ba7e2d0a630b708004e673b629
chogain <chogain05@gmail.com>
2023-09-27 17:36:39 +0900

    study: 삼항연산자 사용하는 로직 추가

--- 19-react-event/src/ex/ChangeText.js
+++ 19-react-event/src/ex/ChangeText.js
@@ -5,13 +5,18 @@ const ChangeText = () => {
   const [btnText, setBtnText] = useState('사라져라');
 
   const handleText = () => {
-    if (btnText === '사라져라') {
-      setBtnText('나타나라');
-      setText('');
-    } else {
-      setBtnText('사라져라');
-      setText('안녕하세요');
-    }
+    // if문
+    // if (btnText === '사라져라') {
+    //   setBtnText('나타나라');
+    //   setText('');
+    // } else {
+    //   setBtnText('사라져라');
+    //   setText('안녕하세요');
+    // }
+
+    // 삼항연산자
+    setText((text) => (text === '' ? '안녕하세요' : ''));
+    setBtnText((text) => (text === '나타나라' ? '사라져라' : '나타나라'));
   };
 
   return (
